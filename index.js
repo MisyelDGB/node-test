b77d1b8dd9f91c07dc1271cacadd4ec792464c2d
MisyelDGB <116567951+MisyelDGB@users.noreply.github.com>
2026-08-10 05:04:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<h1>My Node.js Test App</h1><p>Deployed via GitHub!</p>');
+  res.end('<h1>Misyel's Node.js Test</h1><p>Deployed via GitHub!</p>');
 });
 
 server.listen(process.env.PORT || 3000, () => {
